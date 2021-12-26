720560e1041d161e079175819873a956013daa7b
gökhan yazıcı <gokhanyzc9797@gmail.com>
2021-12-26 18:00:00 +0300

    Add project files.

---
+++ ShopApp.Business/Concrete/CategoryManager.cs
@@ -0,0 +1,47 @@
+﻿using ShopApp.Business.Abstract;
+using ShopApp.DataAccess.Abstract;
+using ShopApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApp.Business.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        private readonly ICategoryDal _categoryDal;
+
+        public CategoryManager(ICategoryDal categoryDal)
+        {
+            _categoryDal = categoryDal;
+        }
+        public void Create(Category entity)
+        {
+            _categoryDal.Create(entity);
+        }
+
+        public void Delete(Category entity)
+        {
+            _categoryDal.Delete(entity);
+
+        }
+
+        public List<Category> GetAll()
+        {
+           return _categoryDal.GetAll().ToList();
+
+        }
+
+        public Category GetById(int id)
+        {
+            return _categoryDal.GetById(id);
+        }
+
+        public void Update(Category entity)
+        {
+            _categoryDal.Update(entity);
+        }
+    }
+}
---
+++ ShopApp.DataAccess/Concrete/EfCore/ShopContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApp.DataAccess.Concrete.EfCore
+{
+    public class ShopContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-ECMROCT;Database=ShopDb;integrated security=true;");
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
